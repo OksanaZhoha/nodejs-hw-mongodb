d6677a69eac0a3a1b91c3044813012ae2162a024
Oksana Zhoha <145046679+OksanaZhoha@users.noreply.github.com>
2024-07-07 19:52:04 +0300

    Initial commit

---
+++ src/scripts/removeLastContact.js
@@ -0,0 +1,18 @@
+import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
+
+export const removeLastContact = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(data);
+
+    if (contacts && contacts.length > 0) {
+      contacts.pop();
+      await fs.writeFile(PATH_DB, JSON.stringify(contacts));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+await removeLastContact();
