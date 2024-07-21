dc4452ecf7c03d3e604e3e1d5c6d2aa4962aaac3
Oksana Zhoha <145046679+OksanaZhoha@users.noreply.github.com>
2024-07-21 18:58:53 +0300

    Create auth.js

---
+++ src/constans/auth.js
@@ -0,0 +1,6 @@
+export const emailReg =
+  /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+export const ACCESS_TOKEN_LIFETIME = 45 * 60 * 1000;
+
+export const REFRESH_TOKEN_LIFETIME = 30 * 24 * 3600 * 1000;
