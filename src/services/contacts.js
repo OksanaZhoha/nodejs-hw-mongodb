2dbdf09b0674313ba34c46c142dada9710ba28c4
Oksana Zhoha <145046679+OksanaZhoha@users.noreply.github.com>
2024-07-21 19:00:15 +0300

    Update contacts.js

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,10 +1,35 @@
 import { ContactsCollection } from '../db/models/contact.js';
 import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
-export const getAllContacts = async ({ page, perPage, sortBy, sortOrder }) => {
+export const getAllContacts = async ({
+  page,
+  perPage,
+  sortBy,
+  sortOrder,
+  contactType,
+  isFavorite,
+  userId,
+}) => {
   const skip = (page - 1) * perPage;
-  const totalItems = await ContactsCollection.countDocuments();
-  const data = await ContactsCollection.find()
+  const dataBaseQuery = ContactsCollection.find();
+
+  if (userId) {
+    dataBaseQuery.where('userId').equals(userId);
+  }
+
+  if (contactType) {
+    dataBaseQuery.where('contactType').equals(contactType);
+  }
+
+  if (isFavorite) {
+    dataBaseQuery.where('isFavorite').equals(isFavorite);
+  }
+
+  const totalItems = await ContactsCollection.find()
+    .merge(dataBaseQuery)
+    .countDocuments();
+
+  const data = await dataBaseQuery
     .skip(skip)
     .limit(perPage)
     .sort({ [sortBy]: sortOrder });
@@ -28,8 +53,8 @@ export const getAllContacts = async ({ page, perPage, sortBy, sortOrder }) => {
   };
 };
 
-export const getContactById = async (contactId) => {
-  const contact = await ContactsCollection.findById(contactId);
+export const getContact = async (filter) => {
+  const contact = await ContactsCollection.findOne(filter);
   return contact;
 };
 
@@ -40,7 +65,6 @@ export const addContact = async (cont) => {
 
 export const updateContact = async (filter, data, options = {}) => {
   const result = await ContactsCollection.findOneAndUpdate(filter, data, {
-    new: true,
     includeResultMetadata: true,
     ...options,
   });
