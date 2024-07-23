63f154459139db46b51453ad84554607b182cacd
Oksana Zhoha <145046679+OksanaZhoha@users.noreply.github.com>
2024-07-23 20:46:22 +0300

    Create uploadFiles.js

---
+++ src/constans/uploadFiles.js
@@ -0,0 +1,7 @@
+import path from 'node:path';
+
+export const TEMP_UPLOAD_DIR = path.resolve('src', 'temp');
+
+export const PUBLIC_DIR = path.resolve('src', 'public');
+
+export const PUBLIC_PHOTO_DIR = path.resolve('src', 'public');
