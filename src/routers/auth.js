175f9ff6beb12350b2452cc8741392b433fe33d5
Oksana Zhoha <145046679+OksanaZhoha@users.noreply.github.com>
2024-07-23 20:46:06 +0300

    Update auth.js

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,11 +1,18 @@
 import { Router } from 'express';
-import { userAddSchema, userSigninSchema } from '../validation/auth.js';
+import {
+  requestResetEmailSchema,
+  resetPasswordSchema,
+  userAddSchema,
+  userSigninSchema,
+} from '../validation/auth.js';
 import { validateBody } from '../utils/validationBody.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import {
   addUserController,
   logoutController,
   refreshController,
+  requestResetEmailController,
+  resetPasswordController,
   signinController,
 } from '../controllers/auth.js';
 
@@ -26,3 +33,15 @@ authRouter.post(
 authRouter.post('/refresh', ctrlWrapper(refreshController));
 
 authRouter.post('/logout', ctrlWrapper(logoutController));
+
+authRouter.post(
+  '/send-reset-email',
+  validateBody(requestResetEmailSchema),
+  ctrlWrapper(requestResetEmailController),
+);
+
+authRouter.post(
+  '/reset-pwd',
+  validateBody(resetPasswordSchema),
+  ctrlWrapper(resetPasswordController),
+);
