737c822839d5fb9ab318dcd7025e72cf49d76586
Oksana Zhoha <145046679+OksanaZhoha@users.noreply.github.com>
2024-07-23 20:45:52 +0300

    Update auth.js

--- src/constans/auth.js
+++ src/constans/auth.js
@@ -4,3 +4,11 @@ export const emailReg =
 export const ACCESS_TOKEN_LIFETIME = 45 * 60 * 1000;
 
 export const REFRESH_TOKEN_LIFETIME = 30 * 24 * 3600 * 1000;
+
+export const SMTP = {
+  SMTP_HOST: 'SMTP_HOST',
+  SMTP_PORT: 'SMTP_PORT',
+  SMTP_USER: 'SMTP_USER',
+  SMTP_PASSWORD: 'SMTP_PASSWORD',
+  SMTP_FROM: 'SMTP_FROM',
+};
